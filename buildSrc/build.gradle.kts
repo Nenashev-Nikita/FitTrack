17ff2b3cb8c694c89e581e169cc883c9828b1d12
nnenashev <n.nenashev@korona.net>
2025-03-30 22:39:03 +0700

    build sustem

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,3 +5,9 @@ plugins {
 repositories {
     mavenCentral()
 }
+
+// Use the build script defined in buildSrc
+apply(from = rootProject.file("buildSrc/shared.gradle.kts"))
+
+// Use the custom task defined in buildSrc
+tasks.register<MyCustomTask>("myCustomTask")
