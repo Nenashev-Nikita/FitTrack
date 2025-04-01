402f906051b85aabb08ff7fd9dcc5bc2bba6d9fc
nnenashev <n.nenashev@korona.net>
2025-04-01 13:37:44 +0700

    Revert "build sustem"
    This reverts commit 17ff2b3cb8c694c89e581e169cc883c9828b1d12.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,9 +5,3 @@ plugins {
 repositories {
     mavenCentral()
 }
-
-// Use the build script defined in buildSrc
-apply(from = rootProject.file("buildSrc/shared.gradle.kts"))
-
-// Use the custom task defined in buildSrc
-tasks.register<MyCustomTask>("myCustomTask")
--- buildSrc/src/main/kotlin/config.gradle.kts
+++
@@ -1,7 +0,0 @@
-repositories {
-	mavenCentral()
-}
-
-dependencies {
-	implementation("org.slf4j:slf4j-api:1.7.32")
-}
