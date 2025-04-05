5758d85c4c8337ceb5ca6e5de6953738c0052bc3
nnenashev <n.nenashev@korona.net>
2025-04-05 11:56:16 +0700

    clear BaseComponent

--- feature/main/src/commonMain/kotlin/ru/fit/app/features/main/presentation/MainComponent.kt
+++ feature/main/src/commonMain/kotlin/ru/fit/app/features/main/presentation/MainComponent.kt
@@ -2,29 +2,34 @@ package ru.fit.app.features.main.presentation
 
 import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.essenty.lifecycle.subscribe
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import ru.fit.app.core.presentation.BaseComponent
+import kotlinx.coroutines.launch
 import ru.fit.app.shared.training.domain.usecase.GetTrainingsUseCase
 
 class MainComponent(
 	componentContext: ComponentContext,
 	val getTrainingsUseCase: GetTrainingsUseCase,
-) : BaseComponent(componentContext) {
+) : ComponentContext by componentContext {
+
+	private val coroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
 
 	private val _screenState = MutableStateFlow<State>(State.Initial)
 	val screenState: StateFlow<State> = _screenState.asStateFlow()
 
 	init {
 		lifecycle.subscribe(
-			onDestroy = { scope.cancel() }
+			onDestroy = { coroutineScope.cancel() }
 		)
 	}
 
 	fun loadContent() {
-		launch {
+		coroutineScope.launch {
 			_screenState.value = State.Loading
 			try {
 				val trainings = getTrainingsUseCase()
